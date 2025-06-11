218056a8047b086e9895f599e22d8040aa03e615
gaurav-anand1603 <gauravanand1603@gmail.com>
2025-06-12 03:14:51 +0530

    Completed User API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -71,6 +72,38 @@ userRouter.get("/user/requests/connections", userAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(400).send(err.message);
+  }
+});
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+    });
+
+    const toHideUsers = new Set();
+    connectionRequest.forEach((data) => {
+      toHideUsers.add(data.toUserId.toString());
+      toHideUsers.add(data.fromUserId.toString());
+    });
+    const users = await User.find({
+      _id: {
+        $nin: [...toHideUsers],
+        $ne: loggedInUser._id,
+      },
+    }).select([
+      "firstName",
+      "lastName",
+      "photoUrl",
+      "age",
+      "gender",
+      "about",
+      "skills",
+    ]);
+
+    res.send(users);
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 });
 
